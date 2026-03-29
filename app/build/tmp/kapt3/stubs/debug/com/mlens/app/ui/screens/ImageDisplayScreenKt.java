af9b45e842898dfc08bff75f33766f72a07c3e86
Yasharth Dwivedi <152445741+YASHARTH-DWIVEDI@users.noreply.github.com>
2026-03-30 03:19:26 +0530

    Add files via upload

---
+++ app/build/tmp/kapt3/stubs/debug/com/mlens/app/ui/screens/ImageDisplayScreenKt.java
@@ -0,0 +1,15 @@
+package com.mlens.app.ui.screens;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000(\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\t\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a \u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0006\u0010\u0006\u001a\u00020\u0007H\u0007\u001a\u0018\u0010\b\u001a\u00020\u00012\u0006\u0010\t\u001a\u00020\n2\u0006\u0010\u000b\u001a\u00020\fH\u0002\u00a8\u0006\r"}, d2 = {"ImageDisplayScreen", "", "navController", "Landroidx/navigation/NavController;", "imageId", "", "repository", "Lcom/mlens/app/data/repository/ImageRepository;", "shareImage", "context", "Landroid/content/Context;", "image", "Lcom/mlens/app/data/database/ImageWithTags;", "app_debug"})
+public final class ImageDisplayScreenKt {
+    
+    @kotlin.OptIn(markerClass = {androidx.compose.material3.ExperimentalMaterial3Api.class})
+    @androidx.compose.runtime.Composable()
+    public static final void ImageDisplayScreen(@org.jetbrains.annotations.NotNull()
+    androidx.navigation.NavController navController, long imageId, @org.jetbrains.annotations.NotNull()
+    com.mlens.app.data.repository.ImageRepository repository) {
+    }
+    
+    private static final void shareImage(android.content.Context context, com.mlens.app.data.database.ImageWithTags image) {
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/mlens/app/ui/screens/TagEditScreenKt.java
@@ -0,0 +1,17 @@
+package com.mlens.app.ui.screens;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u00000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u0003\n\u0002\u0010 \n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\t\n\u0002\b\u0002\u001a[\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0006\u0010\u0006\u001a\u00020\u00072\b\u0010\b\u001a\u0004\u0018\u00010\u00052\b\u0010\t\u001a\u0004\u0018\u00010\u00052\f\u0010\n\u001a\b\u0012\u0004\u0012\u00020\u00050\u000b2\u0006\u0010\f\u001a\u00020\r2\n\b\u0002\u0010\u000e\u001a\u0004\u0018\u00010\u000fH\u0007\u00a2\u0006\u0002\u0010\u0010\u00a8\u0006\u0011"}, d2 = {"TagEditScreen", "", "navController", "Landroidx/navigation/NavController;", "imagePath", "", "isTextBased", "", "extractedText", "summary", "initialTags", "", "repository", "Lcom/mlens/app/data/repository/ImageRepository;", "existingImageId", "", "(Landroidx/navigation/NavController;Ljava/lang/String;ZLjava/lang/String;Ljava/lang/String;Ljava/util/List;Lcom/mlens/app/data/repository/ImageRepository;Ljava/lang/Long;)V", "app_debug"})
+public final class TagEditScreenKt {
+    
+    @kotlin.OptIn(markerClass = {androidx.compose.material3.ExperimentalMaterial3Api.class})
+    @androidx.compose.runtime.Composable()
+    public static final void TagEditScreen(@org.jetbrains.annotations.NotNull()
+    androidx.navigation.NavController navController, @org.jetbrains.annotations.NotNull()
+    java.lang.String imagePath, boolean isTextBased, @org.jetbrains.annotations.Nullable()
+    java.lang.String extractedText, @org.jetbrains.annotations.Nullable()
+    java.lang.String summary, @org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.String> initialTags, @org.jetbrains.annotations.NotNull()
+    com.mlens.app.data.repository.ImageRepository repository, @org.jetbrains.annotations.Nullable()
+    java.lang.Long existingImageId) {
+    }
+}
