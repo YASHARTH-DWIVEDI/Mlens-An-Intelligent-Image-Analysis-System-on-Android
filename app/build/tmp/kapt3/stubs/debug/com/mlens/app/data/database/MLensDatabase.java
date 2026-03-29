eb186339c45fbbcc265e7c5630ebf9015cc1e2ab
Yasharth Dwivedi <152445741+YASHARTH-DWIVEDI@users.noreply.github.com>
2026-03-30 03:11:23 +0530

    Add files via upload

---
+++ app/build/tmp/kapt3/stubs/debug/com/mlens/app/data/database/MLensDatabase.java
@@ -0,0 +1,52 @@
+package com.mlens.app.data.database;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\'\u0018\u0000 \u00052\u00020\u0001:\u0001\u0005B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H&\u00a8\u0006\u0006"}, d2 = {"Lcom/mlens/app/data/database/MLensDatabase;", "Landroidx/room/RoomDatabase;", "()V", "imageDao", "Lcom/mlens/app/data/database/ImageDao;", "Companion", "app_debug"})
+@androidx.room.Database(entities = {com.mlens.app.data.database.ImageEntity.class}, version = 1, exportSchema = false)
+public abstract class MLensDatabase extends androidx.room.RoomDatabase {
+    @kotlin.jvm.Volatile()
+    @org.jetbrains.annotations.Nullable()
+    private static volatile com.mlens.app.data.database.MLensDatabase INSTANCE;
+    @org.jetbrains.annotations.NotNull()
+    private static final androidx.room.migration.Migration MIGRATION_1_2 = null;
+    @org.jetbrains.annotations.NotNull()
+    public static final com.mlens.app.data.database.MLensDatabase.Companion Companion = null;
+    
+    public MLensDatabase() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public abstract com.mlens.app.data.database.ImageDao imageDao();
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u0086\u0003\u0018\u00002\u00020\u0001:\u0001\fB\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\t\u001a\u00020\u00042\u0006\u0010\n\u001a\u00020\u000bR\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0011\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\b\u00a8\u0006\r"}, d2 = {"Lcom/mlens/app/data/database/MLensDatabase$Companion;", "", "()V", "INSTANCE", "Lcom/mlens/app/data/database/MLensDatabase;", "MIGRATION_1_2", "Landroidx/room/migration/Migration;", "getMIGRATION_1_2", "()Landroidx/room/migration/Migration;", "getDatabase", "context", "Landroid/content/Context;", "DatabaseCallback", "app_debug"})
+    public static final class Companion {
+        
+        private Companion() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final com.mlens.app.data.database.MLensDatabase getDatabase(@org.jetbrains.annotations.NotNull()
+        android.content.Context context) {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final androidx.room.migration.Migration getMIGRATION_1_2() {
+            return null;
+        }
+        
+        @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006H\u0016\u00a8\u0006\u0007"}, d2 = {"Lcom/mlens/app/data/database/MLensDatabase$Companion$DatabaseCallback;", "Landroidx/room/RoomDatabase$Callback;", "()V", "onCreate", "", "db", "Landroidx/sqlite/db/SupportSQLiteDatabase;", "app_debug"})
+        static final class DatabaseCallback extends androidx.room.RoomDatabase.Callback {
+            
+            public DatabaseCallback() {
+                super();
+            }
+            
+            @java.lang.Override()
+            public void onCreate(@org.jetbrains.annotations.NotNull()
+            androidx.sqlite.db.SupportSQLiteDatabase db) {
+            }
+        }
+    }
+}
